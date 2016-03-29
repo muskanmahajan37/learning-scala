7acc74c999615d04bfe25a6f742696d767e9de7f
Fengda HUANG <h@phodal.com>
2016-03-29 10:17:26 +0800

    add json serials

--- app/controllers/ApiController.scala
+++ app/controllers/ApiController.scala
@@ -1,18 +1,28 @@
 package controllers
 
+import models.User
 import play.api.libs.json._
 import play.api.mvc.{Action, Controller}
 import play.api.libs.concurrent.Execution.Implicits._
 import services.UserService
 
 class ApiController extends Controller {
+
+  implicit val writesItem = Writes[User] {
+    case User(id, firstName, lastName, mobile, email) =>
+      Json.obj(
+        "id" -> id,
+        "firstName" -> firstName,
+        "lastName" -> lastName,
+        "email" -> email,
+        "mobile" -> mobile
+      )
+  }
+
   def getUser(id: Long) = Action.async { implicit request =>
     UserService.getUser(id).map {
       case None => NotFound(Json.obj("error" -> "Not Found"))
-      case Some(user) => Ok(Json.obj(
-        "firstname" -> user.firstName,
-        "email" -> user.email
-      ))
+      case Some(user) => Ok(Json.toJson(user))
     }
   }
 }
